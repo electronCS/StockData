8977b56e542a616ad2466b354f9e52ae97003988
unknown <metash.ian@gmail.com>
2021-07-24 21:16:04 -0400

    collect data

--- src/main/java/com/company/stockstuff/App.java
+++ src/main/java/com/company/stockstuff/App.java
@@ -1,10 +1,16 @@
 package com.company.stockstuff;
 
+import java.util.TreeMap;
+
 import com.company.stockstuff.dao.S3AccessDao;
+import com.company.stockstuff.data.API;
+import com.company.stockstuff.data.Candle;
 
 import software.amazon.awssdk.regions.Region;
 
 public class App {
+	
+	public static final TreeMap<String,Candle[]> history = new TreeMap<String,Candle[]>();
 	
     public static void main(String[] args) {
     	S3AccessDao.setBucket("stock-data-bucket-ahcc");
@@ -15,6 +21,91 @@ public class App {
 
         String result = S3AccessDao.putS3Object(objectKey,objectPath);
         System.out.println("Tag information: "+result);
+        
+        /*
+        new Thread(()->{
+        	collectData();
+        });
+         */
+    }
+    
+    public static void collectData() {
+    	
+		long targetStartTime = Common.getTimeMillis(); // the time when we next want to sample market data
+		long lastStartTime = targetStartTime;
+		
+		// where in the block are we
+		int historyIndex = (int)(targetStartTime/1000);
+		
+		while(true) {
+			long startTime = Common.getTimeMillis();
+			
+			// every hour, write all blocks to files
+			if(startTime/Common.MILLIS_IN_HOUR>lastStartTime/Common.MILLIS_IN_HOUR) {
+				int endHour = (int)Common.alignToHour(startTime);
+				for(String assetName : history.keySet()) {
+					try {
+						Candle.IO.write(endHour,history.get(assetName),assetName);
+					} catch(Exception e) {
+						e.printStackTrace();
+						System.err.println("sad");
+					}
+				}
+				historyIndex = 0;
+			}
+			
+			try {
+				
+				// collect crypto and stock data
+				TreeMap<String,Candle> candles = new TreeMap<String,Candle>();
+				candles.putAll(API.Crypto.getCurrentCandles());
+				candles.putAll(API.Stocks.getCurrentCandles());
+				
+				// collect some timing/lag info (stored in each sample)
+				long endTime = Common.getTimeMillis();
+				int lag = (int)(endTime-startTime);
+				
+				// add all samples to the latest hour-aligned block for their corresponding assets
+				for(String assetName : candles.keySet()) {
+					Candle[] block = history.get(assetName);
+					
+					// add block if not already existing
+					if(block==null) {
+						block = new Candle[Common.SECOND_IN_HOUR];
+						for(int i=0;i<block.length;i++) {
+							block[i] = new Candle();
+						}
+						history.put(assetName,block);
+					}
+					
+					// put the candle in the block
+					Candle candle = candles.get(assetName);
+					candle.lag = lag;
+					block[historyIndex] = candle;
+				}
+				
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+			lastStartTime = startTime;
+			
+			targetStartTime += 1000;
+			historyIndex++;
+			
+			// try to align api calls with each second
+			try {
+				long endTime = Common.getTimeMillis();
+			
+				int delay = (int)(targetStartTime-endTime);
+				if(delay>0) {
+					Thread.sleep(delay);
+				}
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
     }
     
 }
--- src/main/java/com/company/stockstuff/Common.java
+++ src/main/java/com/company/stockstuff/Common.java
@@ -3,7 +3,9 @@ package com.company.stockstuff;
 import java.util.Date;
 
 public class Common {
-
+	public static final int SECOND_IN_HOUR = 3600;
+	public static final int MILLIS_IN_HOUR = SECOND_IN_HOUR*1000;
+	
 	public static long getTimeMillis() {
 		return new Date().getTime();
 	}
